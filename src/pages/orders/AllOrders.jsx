280040e9353f47112239c66abe7fd9a55b849bee
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-06-13 10:56:35 +0800

    feat: Integrate API in All Order Page

--- src/pages/orders/AllOrders.jsx
+++ src/pages/orders/AllOrders.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa6";
 import Breadcrumb from "../../components/atoms/Breadcrumb";
 import { formatToRp, toCapitalizeCase } from "../../utils/format";
-import productImage from "../../assets/homepage/product-image.png";
-import { Link } from "react-router-dom";
 import IconButton from "../../components/atoms/IconButton";
-import { FaEye } from "react-icons/fa6";
+import useAuth from "../../hooks/useAuth";
+import { getOrdersByUserId } from "../../services/orderService";
 
 const AllOrders = () => {
+  const [ordersData, setOrdersData] = useState([]);
+  const user = useAuth(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getOrdersByUserId(user.id);
+        setOrdersData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const products = ordersData.flatMap((order) =>
+    order.items.map((item) => ({
+      orderId: order.id,
+      status: order.status,
+      ...item.product,
+    }))
+  );
+
+  console.log(products);
+
   const breadcrumbItems = [
     { label: "Beranda", to: "/" },
     { label: "Pesanan", to: null },
@@ -37,122 +65,51 @@ const AllOrders = () => {
               </tr>
             </thead>
             <tbody>
-              <tr className="border-b">
-                <td className="flex flex-col items-center gap-2 p-2 md:flex-row md:gap-3 lg:gap-5">
-                  <img
-                    src={productImage}
-                    alt="IPhone X"
-                    className="h-40 w-40 object-cover"
-                  />
-                  <div className="flex flex-col gap-2">
-                    <h2 className="text-lg font-semibold md:text-xl lg:text-2xl">
-                      IPhone X
-                    </h2>
-                    <p className="text-sm font-medium text-gray-500 md:text-base lg:text-lg">
-                      {toCapitalizeCase("Blue")} | 128GB | RAM 8GB
-                    </p>
-                    <p className="text-sm font-medium text-gray-500 lg:text-base">
-                      Berfungsi Dengan Baik | Produk Lengkap | Kondisi{" "}
-                      {toCapitalizeCase("NORMAL")} | Garansi 30 Hari
-                    </p>
-                    <p className="mt-2 text-base font-semibold md:hidden">
-                      Harga: {formatToRp(5000000)}
-                    </p>
-                    <p className="mt-2 text-base font-semibold md:hidden">
-                      Status: <span className="badge-green">Selesai</span>
-                    </p>
-                  </div>
-                </td>
-                <td className="my-auto hidden h-full items-center p-2 text-center text-base font-medium md:table-cell md:text-lg lg:text-xl">
-                  {formatToRp(5000000)}
-                </td>
-                <td className="hidden text-center md:table-cell">
-                  <p className="badge-green">Selesai</p>
-                </td>
-                <td className="text-center">
-                  <Link to={`/orders/1`}>
-                    <IconButton icon={<FaEye className="text-xl" />} />
-                  </Link>
-                </td>
-              </tr>
-              <tr className="border-b">
-                <td className="flex flex-col items-center gap-2 p-2 md:flex-row md:gap-3 lg:gap-5">
-                  <img
-                    src={productImage}
-                    alt="IPhone X"
-                    className="h-40 w-40 object-cover"
-                  />
-                  <div className="flex flex-col gap-2">
-                    <h2 className="text-lg font-semibold md:text-xl lg:text-2xl">
-                      IPhone X
-                    </h2>
-                    <p className="text-sm font-medium text-gray-500 md:text-base lg:text-lg">
-                      {toCapitalizeCase("Blue")} | 128GB | RAM 8GB
-                    </p>
-                    <p className="text-sm font-medium text-gray-500 lg:text-base">
-                      Berfungsi Dengan Baik | Produk Lengkap | Kondisi{" "}
-                      {toCapitalizeCase("NORMAL")} | Garansi 30 Hari
-                    </p>
-                    <p className="mt-2 text-base font-semibold md:hidden">
-                      Harga: {formatToRp(5000000)}
-                    </p>
-                    <p className="mt-2 text-base font-semibold md:hidden">
-                      Status:{" "}
-                      <span className="badge-yellow">Sedang Dikirim</span>
-                    </p>
-                  </div>
-                </td>
-                <td className="my-auto hidden h-full items-center p-2 text-center text-base font-medium md:table-cell md:text-lg lg:text-xl">
-                  {formatToRp(5000000)}
-                </td>
-                <td className="hidden text-center md:table-cell">
-                  <p className="badge-yellow">Sedang Dikirim</p>
-                </td>
-                <td className="text-center">
-                  <Link to={`/orders/1`}>
-                    <IconButton icon={<FaEye className="text-xl" />} />
-                  </Link>
-                </td>
-              </tr>
-              <tr className="border-b">
-                <td className="flex flex-col items-center gap-2 p-2 md:flex-row md:gap-3 lg:gap-5">
-                  <img
-                    src={productImage}
-                    alt="IPhone X"
-                    className="h-40 w-40 object-cover"
-                  />
-                  <div className="flex flex-col gap-2">
-                    <h2 className="text-lg font-semibold md:text-xl lg:text-2xl">
-                      IPhone X
-                    </h2>
-                    <p className="text-sm font-medium text-gray-500 md:text-base lg:text-lg">
-                      {toCapitalizeCase("Blue")} | 128GB | RAM 8GB
-                    </p>
-                    <p className="text-sm font-medium text-gray-500 lg:text-base">
-                      Berfungsi Dengan Baik | Produk Lengkap | Kondisi{" "}
-                      {toCapitalizeCase("NORMAL")} | Garansi 30 Hari
-                    </p>
-                    <p className="mt-2 text-base font-semibold md:hidden">
-                      Harga: {formatToRp(5000000)}
-                    </p>
-                    <p className="mt-2 text-base font-semibold md:hidden">
-                      Status:{" "}
-                      <span className="badge-yellow">Sedang Dikirim</span>
-                    </p>
-                  </div>
-                </td>
-                <td className="my-auto hidden h-full items-center p-2 text-center text-base font-medium md:table-cell md:text-lg lg:text-xl">
-                  {formatToRp(5000000)}
-                </td>
-                <td className="hidden text-center md:table-cell">
-                  <p className="badge-yellow">Sedang Dikirim</p>
-                </td>
-                <td className="text-center">
-                  <Link to={`orders/1`}>
-                    <IconButton icon={<FaEye className="text-xl" />} />
-                  </Link>
-                </td>
-              </tr>
+              {products.map((product) => {
+                return (
+                  <tr key={product.id} className="border-b">
+                    <td className="flex flex-col items-center gap-2 p-2 md:flex-row md:gap-3 lg:gap-5">
+                      <img
+                        src={product.image_url}
+                        alt={product.title}
+                        className="h-40 w-40 object-cover"
+                      />
+                      <div className="flex flex-col gap-2">
+                        <h2 className="text-lg font-semibold md:text-xl lg:text-2xl">
+                          {product.title}
+                        </h2>
+                        <p className="text-sm font-medium text-gray-500 md:text-base lg:text-lg">
+                          {toCapitalizeCase(product.color)} | {product.storage}{" "}
+                          | RAM {product.ram}
+                        </p>
+                        <p className="text-sm font-medium text-gray-500 lg:text-base">
+                          Berfungsi Dengan Baik | Produk Lengkap | Kondisi{" "}
+                          {toCapitalizeCase(product.condition)}
+                        </p>
+                        <p className="mt-2 text-base font-semibold md:hidden">
+                          Harga: {formatToRp(product.price)}
+                        </p>
+                        <p className="mt-2 text-base font-semibold md:hidden">
+                          Status: <span className="badge-green">{product.status}</span>
+                        </p>
+                      </div>
+                    </td>
+                    <td className="my-auto hidden h-full items-center p-2 text-center text-base font-medium md:table-cell md:text-lg lg:text-xl">
+                      {formatToRp(product.price)}
+                    </td>
+                    <td className="hidden text-center md:table-cell">
+                      <p className={`badge-${product.status === 'PENDING' ? 'yellow' : 'green'}`}>
+                        {product.status}
+                      </p>
+                    </td>
+                    <td className="text-center">
+                      <Link to={`/orders/${product.orderId}`}>
+                        <IconButton icon={<FaEye className="text-xl" />} />
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </section>
